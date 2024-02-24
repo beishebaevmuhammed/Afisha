40411308be2ed6b907f2b3c1426315682dfe31d0
beishebaevmuhammed <muhammedbeishebaev3@gmail.com>
2024-02-24 12:34:13 +0600

    hw2

--- movie_app/models.py
+++ movie_app/models.py
@@ -8,18 +8,30 @@ class Director(models.Model):
         return self.name
 
 
+
+
+
+
 class Movie(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField(blank=True)
     duration = models.CharField(max_length=20)
     director = models.ForeignKey(Director, on_delete=models.CASCADE)
+    movie = models.ForeignKey('self', on_delete=models.CASCADE, null=True,
+                              blank=True, related_name='movies')
 
     def __str__(self):
         return self.title
 
+
 class Review(models.Model):
     text = models.TextField(blank=True)
-    movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
-
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE,
+                              related_name='reviews')
+    stars = models.IntegerField(
+        verbose_name='Звезды',
+        help_text='Рейтинг отзыва от 1 до 5',
+        choices=[(i, i) for i in range(1, 6)],  # Ограничивает значение от 1 до 5
+        default=1,)
     def __str__(self):
         return self.text
--- movie_app/serializer.py
+++ movie_app/serializer.py
@@ -1,18 +1,36 @@
 from rest_framework import serializers
 from .models import Director, Movie, Review
 
+
 class DirectorSerializer(serializers.ModelSerializer):
+    movies_count = serializers.SerializerMethodField()
+
     class Meta:
         model = Director
         fields = '__all__'
 
-class MovieSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Movie
-        fields = '__all__'
+    def get_movies_count(self, obj):
+        return obj.movies.count()
+
 
 class ReviewSerializer(serializers.ModelSerializer):
     class Meta:
         model = Review
         fields = '__all__'
 
+
+class MovieSerializer(serializers.ModelSerializer):
+    reviews = ReviewSerializer(many=True, read_only=True)
+    average_rating = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Movie
+        fields = '__all__'
+
+    def get_average_rating(self, obj):
+        total_stars = sum(review.stars for review in obj.reviews.all())
+        num_reviews = obj.reviews.count()
+        if total_stars > 0:
+            return total_stars / num_reviews
+        else:
+            return 0.0
